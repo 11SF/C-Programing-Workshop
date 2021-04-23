abbca9bc74418a63b06770bb560dafd77862790e
11SF <nsfolknsf@gmail.com>
2021-04-23 22:38:48 +0700

    ex 1

---
+++ display.c
@@ -0,0 +1,16 @@
+#include<stdio.h> // import library : standard input output
+int main() 
+{
+    int a;
+    float b;
+    double c;
+    scanf("%d %f %lf",&a, &b, &c);
+    /*
+    %d  : int
+    %f  : float
+    %lf : double
+    */
+    printf("integer is \t%d \nfloat is \t%f \ndouble is \t%.3lf\n",a,b,c);
+    // \t : tab
+    // \n : new line
+}
