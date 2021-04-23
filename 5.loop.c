cea3e00968b4d4c585495df965edd406fa5eec6b
11SF <nsfolknsf@gmail.com>
2021-04-24 03:07:22 +0700

    init

---
+++ 5.loop.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() 
+{
+    int count = 0;
+    // while loop
+    while(count < 10) {
+        printf("%d\n",count);
+
+        count++;
+
+        // 2 x 1 = 2
+        // 2 x 2 = 4
+        // 2 x 3 = 6
+        // 2 x 4 = 8
+        
+
+
+    }
+    // do while loop
+
+    // for loop
+
+}
