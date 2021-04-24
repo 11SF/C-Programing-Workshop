16ced7e625361d111fecee57b645614932af5610
11SF <nsfolknsf@gmail.com>
2021-04-25 01:43:11 +0700

    added dowhile and for loop

--- 5.loop.c
+++ 5.loop.c
@@ -2,22 +2,38 @@
 int main() 
 {
     int count = 0;
+    int input;
+
     // while loop
-    while(count < 10) {
-        printf("%d\n",count);
+    // while(count < 10) {
+    //     printf("%d\n",count);
 
-        count++;
+    //     count++;
 
-        // 2 x 1 = 2
-        // 2 x 2 = 4
-        // 2 x 3 = 6
-        // 2 x 4 = 8
-        
+    //     // 2 x 1 = 2
+    //     // 2 x 2 = 4
+    //     // 2 x 3 = 6
+    //     // 2 x 4 = 8
+    // }
 
-
-    }
     // do while loop
+    // do {
+
+    //     printf("Please enter input 2 - 12 :");
+    //     scanf("%d",&input);
+
+    // } while(input < 2 || input > 12);
+
+    // while(count < 12) {
+    //     printf("%d X %d = %d\n",input,count+1,input*(count+1));
+    //     count++;
+    // }
 
     // for loop
+    // int i;
+
+    for(int i = 0; i < 10; i++) {
+        printf("%d\n",i);
+    }
 
 }
